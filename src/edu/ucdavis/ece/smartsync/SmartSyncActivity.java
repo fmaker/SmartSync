a15b708cc9e812061989007e7f5b76ca11534b7d
Frank Maker <frank.maker@gmail.com>
2011-09-02 13:15:08 -0700

    Hello World Android application

---
+++ src/edu/ucdavis/ece/smartsync/SmartSyncActivity.java
@@ -0,0 +1,13 @@
+package edu.ucdavis.ece.smartsync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmartSyncActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
