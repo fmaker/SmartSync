4711aa63e27b49336b0a7958cb32ef72bc676b1f
Frank Maker <frank.maker@gmail.com>
2011-09-03 12:59:46 -0700

    UserProfile constructor

--- src/edu/ucdavis/ece/smartsync/SmartSyncActivity.java
+++ src/edu/ucdavis/ece/smartsync/SmartSyncActivity.java
@@ -1,7 +1,11 @@
 package edu.ucdavis.ece.smartsync;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.provider.CallLog.Calls;
 import android.util.Log;
 
 import com.android.internal.os.PowerProfile;
@@ -10,6 +14,7 @@ public class SmartSyncActivity extends Activity {
 	private static final String TAG = "SmartSyncActivity";
 	
 	public static PowerProfile sPowerProfile;
+	public static UserProfile sUserProfile;
 	
     /** Called when the activity is first created. */
     @Override
@@ -17,8 +22,10 @@ public class SmartSyncActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        sPowerProfile = new PowerProfile(this);
+        sPowerProfile = new PowerProfile(getApplicationContext());        
+        sUserProfile = new UserProfile(getApplicationContext());
         
         Log.v(TAG,String.format("Battery Capacity: %.0f mAh", sPowerProfile.getBatteryCapacity()));
-    }
+
+     }
 }
