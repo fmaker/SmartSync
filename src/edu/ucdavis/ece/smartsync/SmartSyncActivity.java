85f7c2256e91b3ba0bf5ad156a8952198974b763
Frank Maker <frank.maker@gmail.com>
2011-09-02 22:04:35 -0700

    Able to report battery capacity

--- src/edu/ucdavis/ece/smartsync/SmartSyncActivity.java
+++ src/edu/ucdavis/ece/smartsync/SmartSyncActivity.java
@@ -2,12 +2,23 @@ package edu.ucdavis.ece.smartsync;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.internal.os.PowerProfile;
 
 public class SmartSyncActivity extends Activity {
+	private static final String TAG = "SmartSyncActivity";
+	
+	public static PowerProfile sPowerProfile;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        sPowerProfile = new PowerProfile(this);
+        
+        Log.v(TAG,String.format("Battery Capacity: %.0f mAh", sPowerProfile.getBatteryCapacity()));
     }
 }
